6bf1bb77a908bd8d5ead7f2252ca9d8e3727fc81
Dmitriy <lifeviev@gmai.com>
2017-07-28 17:19:06 +0300

    Обновление второй строки №3

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Привет мир");
-		System.out.println("Обновляем вторую строку");
+		System.out.println("Обновление второй строки №3");
 		System.out.println("Добавляем третью строку");
 		System.out.println("Четвертое обновление");
 
